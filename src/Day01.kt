fbb4c651d07dc5f9fcea58726a235f86f79b2892
Jeff Davidson <jpd236@cornell.edu>
2021-11-30 21:15:19 -0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,21 @@
 fun main() {
     fun part1(input: List<String>) {
-
+        println(input.fold(0 to Int.MAX_VALUE) { (increases, last), value ->
+            (if (value.toInt() > last) increases + 1 else increases) to value.toInt()
+        }.first)
     }
 
     fun part2(input: List<String>) {
-
+        println(input.fold(0 to listOf<Int>()) { (increases, prev), value ->
+            if (prev.size < 3) {
+                increases to prev + value.toInt()
+            } else {
+                val curSum = prev.sum()
+                val nextList = prev.slice(1..2) + value.toInt()
+                val nextSum = nextList.sum()
+                (if (nextSum > curSum) increases + 1 else increases) to nextList
+            }
+        }.first)
     }
 
     // test if implementation meets criteria from the description, like:
